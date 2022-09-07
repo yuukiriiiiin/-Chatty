0349ce2c2981425e4d4c6d7f1ec5518bc9729faa
yuki sakata <sakata_yuuki@waku-2.com>
2022-09-07 20:40:04 +0900

    モーダルのアニメーションを追加

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from './Modal.module.scss'
 import cx from 'classnames'
 
@@ -8,10 +8,41 @@ type Props = {
 }
 
 export const Modal:React.FC<Props> = ({showModal, closeModal}) => {
-  const isDisplay = showModal ? 'block' : 'none';
+  const [full, setFull] = useState(false)
+  const ref = useRef<HTMLDivElement>(null);
+
+  let startY: number;
+  let endY: number;
+
+  const logSwipeStart = (event) => {
+    startY = event.touches[0].pageY;
+  }
+
+  const logSwipe = (event) => {
+    endY = event.touches[0].pageY;
+  }
+
+  const logSwipeEnd = (event) => {
+    if( 0 < (endY - startY) ) {
+      console.log("下向き");
+    } else {
+      console.log("上向き");
+      setFull(true)
+    }
+  }
+  useEffect(() => {
+    ref.current?.addEventListener("touchstart", logSwipeStart, { passive: false });
+    ref.current?.addEventListener("touchmove", logSwipe, { passive: false });
+    ref.current?.addEventListener("touchend", logSwipeEnd, { passive: false });
+    return (() => {
+      ref.current?.removeEventListener("touchstart", logSwipeEnd);
+      ref.current?.removeEventListener("touchmove", logSwipe);
+      ref.current?.removeEventListener("touchend", logSwipeEnd);
+    });
+  });
   return (
     <div className={cx(styles.overlay, showModal ? styles['is-active']: '')}>
-      <div className={cx(styles.modal, showModal ? styles['is-active']: '')}>
+      <div className={cx(styles.modal, showModal ? styles['is-active']: '', full ? styles['full'] : '')}>
         <p>This is ModalContent</p>
         <button onClick={closeModal}>Close</button>
       </div>
